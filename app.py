7c13a85943f8449cc952c15a050430709ad55957
Billy Fung <billy@billyfung.com>
2018-04-04 16:53:28 +1200

    adding in proper requiremnets for code to work

--- app.py
+++ app.py
@@ -3,15 +3,42 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://billy@localhost/testdb'
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
 
 class Tariff(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    metadata = db.Column(db.JSON(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    description = db.Column(db.String(120), unique=True, nullable=False)
+    tariff_metadata = db.Column(db.JSON(80), unique=False, nullable=True)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<Tariff %r>' % self.description
+
+    def __init__(self, description, tariff_metadata):
+        self.description = description
+        self.tariff_metadata = tariff_metadata
+
+
+@app.route("/tariff", methods=["POST"])
+def add_tariff():
+    description = request.json['description']
+    tariff_metadata = request.json['tariff_metadata']
+
+    new_tariff = Tariff(description, tariff_metadata)
+
+    db.session.add(Tariff)
+    db.session.commit()
+
+    return jsonify(new_tariff)
+
+
+@app.route("/tariff", methods=["GET"])
+def get_all_tariffs():
+    all_tariffs = Tariff.query.all()
+    return jsonify(all_tariffs)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
