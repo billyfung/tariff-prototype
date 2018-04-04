f8777f48eaf61212033d5547af8bf7f6f11c667e
Billy Fung <billy@billyfung.com>
2018-04-05 09:32:18 +1200

    post and get works, need to have testdb on local

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ ma = Marshmallow(app)
 
 class Tariff(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    description = db.Column(db.String(120), unique=True, nullable=False)
-    tariff_metadata = db.Column(db.JSON(80), unique=False, nullable=True)
+    description = db.Column(db.String(120))
+    tariff_metadata = db.Column(db.JSON(80))
 
     def __repr__(self):
         return '<Tariff %r>' % self.description
@@ -23,21 +23,23 @@ class Tariff(db.Model):
 
 @app.route("/tariff", methods=["POST"])
 def add_tariff():
+    print(request.json)
     description = request.json['description']
     tariff_metadata = request.json['tariff_metadata']
 
     new_tariff = Tariff(description, tariff_metadata)
 
-    db.session.add(Tariff)
+    db.session.add(new_tariff)
     db.session.commit()
 
-    return jsonify(new_tariff)
+    return jsonify(new_tariff.description)
 
 
 @app.route("/tariff", methods=["GET"])
 def get_all_tariffs():
     all_tariffs = Tariff.query.all()
-    return jsonify(all_tariffs)
+
+    return jsonify([tariff.description for tariff in all_tariffs])
 
 
 if __name__ == '__main__':
